3d7d09e666a7682fc2bba344e4857db2fb90f141
Iulian Onofrei <iulian.onofrei@yahoo.com>
2016-09-28 09:18:55 +0300

    Add install script

---
+++ install/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/Library/Services
+cd ~/Library/Services
+
+git clone https://github.com/revolter/macOS-Automator-Services
+
+cp -r macOS-Automator-Services/Delete\ file\(s\)\ permanently.workflow .
+
